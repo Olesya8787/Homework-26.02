da26957f0e66c9fa54b51249a126bbea4052c3d6
Olesya8787 <vikol.murashko@gmail.com>
2023-02-24 15:38:10 +0200

    Add a new homework

---
+++ app.py
@@ -0,0 +1,9 @@
+print("Hello")
+user_name = input( "Enter your name : " )
+print("Hello" + user_name)
+
+user_email = input( "Enter your email : " )
+print(user_email)
+
+user_year = input( "Enter your year of birth: " )
+print(user_year)
