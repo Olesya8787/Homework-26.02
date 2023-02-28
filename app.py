f8c7632cb8617f46dafcbe171875d20cda658ad9
Olesya8787 <vikol.murashko@gmail.com>
2023-02-28 19:57:52 +0200

    Add a new homework

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-print("Hello")
-user_name = input( "Enter your name : " )
-print("Hello" + user_name)
+user = {
+    "name" : "John",
+    "age" : 23,
+    "friends" : [ 
+        "Mike", "Bob" , "Joe"
+    ],
+    "skills" : ("HTML", "CSS", "JS")
+}
+count = user["friends"].count('Mike')
+print(count)
+if count >0 :
+    newUser = {
+       "name" : "Mike"
+    }  
+    print(newUser)
 
-user_email = input( "Enter your email : " )
-print(user_email)
+    user_age = input("Enter your age : ")
+    result = 2023 - int(user_age)
+    print(result)
 
-user_year = input( "Enter your year of birth: " )
-print(user_year)
+del user ["skills"]
+print(user)
+user["skills"] = ["Python" , "QA"  , "Selenium"]
+print(user)
+
+sorted_arr = sorted(user ["friends"] )
+print(sorted_arr)
+
+Bob_index = (user["friends"]).index("Bob")
+print(Bob_index)
+
+user["friends"].append( "Taras ")
+user["friends"].append( "Danya ")
+user["friends"].append( "Bidden ")
+print(user["friends"])
+
+skills = len(user["skills"])
+print(skills)    
